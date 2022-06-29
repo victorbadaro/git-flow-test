3b407cdaceb0ac31c16d1ea6b79694c228474dd8
Victor Badaró <victor.badaro@sensedia.com>
2022-06-29 09:24:44 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Header } from './components/Header';
 import { AppRoutes } from './routes';
 import './styles/global.css';
 
 export function App() {
   return (
     <Router>
+      <Header />
       <AppRoutes />
     </Router>
   );
